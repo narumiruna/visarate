6180c1b564ebb430b9429faba24e9522277f4f1d
narumi <toucans-cutouts0f@icloud.com>
2024-07-25 13:29:25 +0800

    update test case

--- tests/test_rates.py
+++ tests/test_rates.py
@@ -1,19 +1,23 @@
+import pytest
+
 from visarate.rate import query_rate
 
 
-def test_query_rate():
-    cases = [
+@pytest.mark.parametrize(
+    "amount, from_curr, to_curr, fee",
+    [
         [1.0, "TWD", "USD", 0.015],
         [1_000_000.0, "TWD", "USD", 0.015],
         [1.0, "USD", "TWD", 0.015],
         [1_000_000.0, "USD", "TWD", 0.015],
-    ]
-    for amount, from_curr, to_curr, fee in cases:
-        resp = query_rate(amount=amount, from_curr=from_curr, to_curr=to_curr, fee=fee)
-        assert resp.original_values.from_amount == amount
-        assert resp.original_values.from_currency == to_curr
-        assert resp.original_values.to_currency == from_curr
-        assert resp.conversion_amount_value == amount
-        assert resp.conversion_from_currency == from_curr
-        assert resp.conversion_to_currency == to_curr
-        assert resp.conversion_bank_fee == fee
+    ],
+)
+def test_query_rate2(amount: float, from_curr: str, to_curr: str, fee: float):
+    resp = query_rate(amount=amount, from_curr=from_curr, to_curr=to_curr, fee=fee)
+    assert resp.original_values.from_amount == amount
+    assert resp.original_values.from_currency == to_curr
+    assert resp.original_values.to_currency == from_curr
+    assert resp.conversion_amount_value == amount
+    assert resp.conversion_from_currency == from_curr
+    assert resp.conversion_to_currency == to_curr
+    assert resp.conversion_bank_fee == fee
