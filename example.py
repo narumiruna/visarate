e793d6b17f811a330ff03e5591bd324367abd925
narumi <toucans-cutouts0f@icloud.com>
2026-03-01 22:10:29 +0800

    Update README and example.py to streamline usage instructions and improve code clarity

--- example.py
+++ example.py
@@ -5,18 +5,8 @@ from visarate import query_rate
 
 def main() -> None:
     resp = query_rate(
-        amount=1,
-        quote_currency="TWD",  # quote currency
-        base_currency="USD",  # base currency
-        fee=0.0,
-    )
-    print(resp)
-
-    resp = query_rate(
-        amount=1,
-        quote_currency="USD",
-        base_currency="TWD",
-        fee=0.0,
+        base_currency="USD",
+        quote_currency="TWD",
     )
     print(resp)
 
